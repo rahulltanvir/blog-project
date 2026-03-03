aaf969bd6a2b7427cc3cf0b42d60da622a620a71
rahulltanvir <tanvirrahul00000@gmail.com>
2026-03-04 01:04:03 +0600

    Code replace

--- admin/view/addpost_view.php
+++ admin/view/addpost_view.php
@@ -1,14 +1,47 @@
+<?php
+     $catMsg= $adminLoginobj->displayCat();
+
+?>
+
 <h2>Add Post Page</h2>
 
 <form class="form " action="" method="post">
-    <label for="uploadimg">Upload Image:</label>
-    <input type="file" name="" id="uploadimg"><br>
-    <label for="blogName">Post Title</label>
-    <input class="form form-control" type="text" name="cat_name" id="blogName">
-    <label for="blogName">Post Content</label>
-    <input class="form form-control" type="text" name="cat_name" id="blogName">
-    <label for="desBlog">Post Category</label>
-    <input class="form form-control" type="text" name="cat_des" id="desBlog">
-
-    <input class="btn btn-info" style="margin-top:20px;" type="submit" name="add_cat" id="" value="Add Categoty">
+    <div class=" form-group ">
+        <label for="">Upload Image:</label><Br>
+        <input type="file" name="" id="">
+    </div>
+    <div class=" form-group ">
+        <label for="">Post Title:</label><Br>
+        <input class="form-control" type="text" name="Post_title" id="">
+    </div>
+    <div class=" form-group ">
+        <label for="">Post Content:</label><Br>
+        <textarea class="form-control" name="" id=""></textarea>
+    </div>
+    <div class=" form-group ">
+        <label for="">Post Category:</label><Br>
+        <select class="form-control" name="" id="">
+            <?php while($catMsg_f=mysqli_fetch_assoc($catMsg)){ ?>
+            <option value="<?php echo $catMsg_f['cat_id'] ?>">
+                <?php echo $catMsg_f['cat_name'] ?>
+            </option>
+            <?php } ?>
+        </select>
+        
+    </div>
+    <div class=" form-group ">
+        <label for="">Post Summary:</label><Br>
+        <input class="form-control" type="text" name="Post_title" id="">
+    </div>
+    <div class=" form-group ">
+        <label for="">Post Tag:</label><Br>
+        <input class="form-control" type="text" name="Post_title" id="">
+    </div>
+     <div class=" form-group ">
+        <label for="">Post Status:</label><Br>
+        <select name="" id="">
+            <option value="1">Published</option>
+            <option value="0">Unpublish</option>
+        </select>
+    </div>
 </form>
