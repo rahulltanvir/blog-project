031ec29d636b01b64e016749a645ccc08870b940
rahulltanvir <tanvirrahul00000@gmail.com>
2026-03-04 00:21:22 +0600

    add post

--- admin/view/addpost_view.php
+++ admin/view/addpost_view.php
@@ -1,1 +1,14 @@
 <h2>Add Post Page</h2>
+
+<form class="form " action="" method="post">
+    <label for="uploadimg">Upload Image:</label>
+    <input type="file" name="" id="uploadimg"><br>
+    <label for="blogName">Post Title</label>
+    <input class="form form-control" type="text" name="cat_name" id="blogName">
+    <label for="blogName">Post Content</label>
+    <input class="form form-control" type="text" name="cat_name" id="blogName">
+    <label for="desBlog">Post Category</label>
+    <input class="form form-control" type="text" name="cat_des" id="desBlog">
+
+    <input class="btn btn-info" style="margin-top:20px;" type="submit" name="add_cat" id="" value="Add Categoty">
+</form>
